448777101acc44e640f612aa2550bcd2662bf2a3
aquibajani <aquib.ajani@obsidi.com>
2026-07-31 11:58:47 -0400

    Modify User Service

--- Backend/feedApp/src/main/java/com/bptn/feedApp/service/UserService.java
+++ Backend/feedApp/src/main/java/com/bptn/feedApp/service/UserService.java
@@ -3,24 +3,27 @@ package com.bptn.feedApp.service;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.bptn.feedApp.jdbc.UserDao;
+import com.bptn.feedApp.repository.UserRepository;
 import com.bptn.feedApp.jdbc.UserBean;
 import java.util.List;
+import com.bptn.feedApp.jpa.User;
+import java.util.Optional;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	UserDao userDao;
-
-	public List<UserBean> listUsers() {
-		return this.userDao.listUsers();
+	UserRepository userRepository;
+	
+	public List<User> listUsers() {
+		return this.userRepository.findAll();
 	}
 	
-	public UserBean findByUsername(String username) {
-		return this.userDao.findByUsername(username);
+	public Optional<User> findByUsername(String username) {
+		return this.userRepository.findByUsername(username);
 	}
 	
-	public void createUser(UserBean user) {
-		this.userDao.createUser(user);
+	public void createUser(User user) {
+		this.userRepository.save(user);
 	}
 }
